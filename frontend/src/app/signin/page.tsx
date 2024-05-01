9a1a7bb521c0df0b093f5a07c0c6addeba01322c
cinicDiver <oscarframirezp@hotmail.com>
2024-05-01 10:58:47 -0500

    Created sign in form page layout and added some styling.

--- frontend/src/app/signin/page.tsx
+++ frontend/src/app/signin/page.tsx
@@ -1,3 +1,35 @@
+import Link from "next/link";
+
 export default function Page() {
-  return <h1>Sign in</h1>;
+  return (
+    <div>
+      <div className="signin-container">
+        <div className="signin-header">
+          <h1>SkillSync</h1>
+        </div>
+        <div className="signin-body">
+          <h2>Sign in form</h2>
+          <div className="signin-form">
+            <input type="email" placeholder="Email address" />
+            <div>
+              <input type="password" placeholder="Password" />
+              <input type="checkbox" />
+            </div>
+            <div>
+              <input type="password" placeholder="Confirm password" />
+              <input type="checkbox" />
+            </div>
+          </div>
+          <div>
+            <button type="button">Register</button>
+          </div>
+        </div>
+        <div className="signin-footer">
+          <p>
+            Already have an account? <Link href="/login">Log in!</Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
