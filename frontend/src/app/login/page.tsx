d6b8a181925c66c23dbb20eb71bd47b252b8948e
cinicDiver <oscarframirezp@hotmail.com>
2024-05-06 19:05:32 -0500

    Created log in form in login/page.tsx

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,3 +1,29 @@
+import Link from "next/link";
+
 export default function Page() {
-  return <h1>Log in</h1>;
+  return (
+    <div className="signin-container">
+      <div className="signin-header">
+        <h1>SkillSync</h1>
+      </div>
+      <div className="signin-body">
+        <div className="signin-form">
+          <div>
+            <input type="email" placeholder="Email" />
+          </div>
+          <div>
+            <input type="password" placeholder="Password" />
+          </div>
+        </div>
+        <div>
+          <button type="button">Log in</button>
+        </div>
+      </div>
+      <div className="signin-footer">
+        <p>
+          Not a member yet? <Link href="/signin">Register!</Link>
+        </p>
+      </div>
+    </div>
+  );
 }
