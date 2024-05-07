873bf19a4c643c0c9bcaf1b89f8c533c7d192599
cinicDiver <oscarframirezp@hotmail.com>
2024-05-06 20:25:16 -0500

    Added passwrod recovery page.tsx

--- frontend/src/app/password/page.tsx
+++ frontend/src/app/password/page.tsx
@@ -1,3 +1,21 @@
+import Link from "next/link";
+
 export default function Page() {
-  return <h1>Password Recovery</h1>;
+  return (
+    <div className="auth-container">
+      <div className="auth-header">
+        <h1>SkillSync</h1>
+      </div>
+      <div className="auth-body">
+        <div className="auth-form">
+          <input type="text" placeholder="Username" />
+          <input type="email" placeholder="Email" />
+          <div>
+            <button type="button">Recover</button>
+          </div>
+        </div>
+      </div>
+      <div className="auth-footer"></div>
+    </div>
+  );
 }
